44de92278c126599da19f4ce2837c6a8f5ebc606
Leferchian <leferchian@gmail.com>
2023-02-26 02:27:22 +0300

    change code

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,41 +1,41 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+this.subject = subjectName;
 }
 
 Student.prototype.addMark = function (mark) {
-  if (this.marks === undefined) {
-    this.marks.push (mark);
+  if(this.marks === undefined){
+    this.marks = [mark];   
+  } else {
+    this.marks.push(mark);     
   }
-  else {
-    this.marks.push (mark);    
-  }
-  
 }
 
-Student.prototype.addMarks = function(...marks) {
-  if (this.excluded === undefined) {
-    this.marks.push(...marks);
-  }
-}
-  
-Student.prototype.getAverage = function() {
-  if (this.marks === undefined || this.marks.length === 0) {
-    return 0;
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){
+    this.marks = [...mark]; 
   } else {
-      return this.marks.reduce((accum, current) => accum + current, 0) / this.marks.length;
-  }    
+    this.marks.push(...mark);     
+  }  
+}
+
+Student.prototype.getAverage = function() {
+if (this.marks === undefined || this.marks.length === 0) {
+  return 0;
+} else {
+    return this.marks.reduce((accum, current) => accum + current, 0) / this.marks.length;
+}    
 }
 
 Student.prototype.exclude = function(reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+delete this.subject;
+delete this.marks;
+this.excluded = reason;
 }
 // ваш код для остальных методов
